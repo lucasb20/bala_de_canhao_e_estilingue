26a2f253bed415902df3f04da00f74e53033b0c9
Lucas Rocha <lr359508@gmail.com>
2023-05-26 17:35:24 -0300

    Criação do movimento.

--- scripts/main.js
+++ scripts/main.js
@@ -3,21 +3,52 @@ let ctx = canvas.getContext('2d')
 canvas.style.background = 'darkkhaki'
 
 let but_fire = document.querySelector('#but_fire')
-but_fire.addEventListener('click',init) //teste
+but_fire.addEventListener('click',fire)
 
-class ball {
-    constructor(sx, sy, rad, stylestring) {
-        this.sx = sx
-        this.sy = sy
-        this.rad = rad
-        this.draw = drawball
-        this.moveit = moveball
-        this.fillStyle = stylestring
-    }
-}
+let ballrad=5
+let ballx=1+ballrad,bally=149-ballrad
+
+let theta, numv
+
+let v_x,v_y
 
 function init(){
-    ctx.fillStyle = "rgb(255,0,0)"
+    ctx.beginPath()
+    ctx.fillStyle = "#ff0000"
+    ctx.arc(ballx,bally,ballrad,0,Math.PI*2,false)
+    ctx.fill()
+}
+
+function fire(){
+    numv = Number((document.getElementById('numv')).value)
+    theta = (Number((document.getElementById('theta')).value))*Math.PI/180
+
+    v_x = numv*Math.cos(theta)
+    v_y = numv*Math.sin(theta)
+
+    setInterval(move_ball,50)
+}
+
+function move_ball(){
+    ctx.clearRect(0,0,300,150)
+    move_and_check()
+    ctx.beginPath()
+    ctx.arc(ballx,bally,ballrad,0,Math.PI*2,false)
+    ctx.fill()
+}
+
+function move_and_check(){
+    let not_ballx = ballx+v_x
+    let not_bally = bally-v_y
+
+    
+
+    ballx = not_ballx
+    bally = not_bally
+}
+
+/* 
+ctx.fillStyle = "rgb(255,0,0)"
     ctx.save()
     ctx.translate(50,50)
     ctx.rotate(-Math.PI/6)
@@ -25,5 +56,4 @@ function init(){
     ctx.fillRect(50,50,50,50)
     ctx.restore()
     ctx.fillStyle = "rgb(0,0,255)"
-    ctx.fillRect(50,50,5,5)
-}
+    ctx.fillRect(50,50,5,5) */
